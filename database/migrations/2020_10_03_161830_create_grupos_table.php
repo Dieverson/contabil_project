933c4312df4cc18a6db382f2c66fda4b08ac23eb
darlan <sistemasdarlan@gmail.com>
2020-10-03 19:23:40 -0300

    adição plano de contas banco de dados

--- database/migrations/2020_10_03_161830_create_grupos_table.php
+++ database/migrations/2020_10_03_161830_create_grupos_table.php
@@ -17,9 +17,12 @@ class CreateGruposTable extends Migration
             $table->id('idgrupos');
             $table->string('numero_da_conta');
             $table->unsignedBigInteger('natureza_idnatureza');
+            $table->unsignedBigInteger('id_plano_de_contas');
             $table->string('nome_da_conta');
             $table->timestamps();
             $table->foreign('natureza_idnatureza')->references('idnatureza')->on('naturezas');
+            $table->foreign('id_plano_de_contas')->references('id_plano_de_conta')->on('plano_de_contas');
+            
         });
     }
 
